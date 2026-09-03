c03e39b22aec3cbd0cd9c1d31959488fd2927c11
ryi01 <kkjmo3@naver.com>
2026-09-03 09:12:58 +0900

    [Feat] 초기 세팅 및 SubwayData생성
    1. 프로젝트 초기 셋팅
    2. SubwayData로 CSV에 있는 정보를 받을 구조체 생성

---
+++ SubwayData.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <string>
+
+// CSV 파일에서 파싱할 구조체
+struct SubwayData
+{
+    // 라인
+    int line;
+
+    // 출발지
+    std::string toStationName;
+
+    // 도착지
+    std::string fromStationName;
+
+    // 시간 => 초로 저장
+    int timeToSec;
+};
